f4cc71a7b276ce2a4339c24316b41d935c324b27
Yashwanth9394 <yashwanth753@gmail.com>
2025-10-23 15:07:14 -0400

    Initial commit

---
+++ test/accessvault-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AccessvaultInfra from '../lib/accessvault-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/accessvault-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AccessvaultInfra.AccessvaultInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
